432f2fc6d738f9b5e08149ef0f452056e9d22a5e
Kiara Alfaro <arcealfarokiara@gmail.com>
2022-09-28 21:00:05 -0300

    agregar libreias

---
+++ js/arq.js
@@ -0,0 +1,45 @@
+
+    email = prompt ('Si quiere que un Arquitecto se comunique con usted para controlar su compra, dejenos su e-mail')
+
+    arquitecto = prompt( 'Elije a uno de nuestros Arquitectos para su proyecto:\n(ingrese el numero)\n1-Agustin Lazaro\n2-Lidia Gonzales\n3-Alenjandro Diaz\n4-Kiara Alfaro')
+
+
+    switch (arquitecto){
+
+        case ('1'):
+    
+            alert(' El Arq.Agustin Lazaro se comunicara proximamente a '+email+'')
+            alert ('Gracias '+nombre+' por usar ARKI!')
+
+        break
+    
+        case ('2'):
+    
+            alert(' La Arq.Lidia Gonzales se comunicara proximamente a '+email+'')
+            alert ('Gracias '+nombre+' por usar ARKI!')
+            
+        break
+    
+        case ('3'):
+    
+            alert(' El Arq.Alenjandro Diaz se comunicara proximamente a '+email+'')
+            alert ('Gracias '+nombre+' por usar ARKI!')
+            
+        break
+    
+        case ('4'):
+    
+            alert(' La Arq.Kiara Alfaro se comunicara proximamente a '+email+'')
+            alert ('Gracias '+nombre+' por usar ARKI!')
+            
+        break
+    
+        alert ('Gracias '+nombre+' por usar ARKI!')
+    }
+
+
+    alert ('Gracias por usar ARKI!')
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,53 +1,219 @@
+const inputNombre = document.getElementById('nombre')
+const inputApellido = document.getElementById('apellido')
+const botonIngresar= document.getElementById('ingresar')
+const divDatos = document.querySelector('#divDatos')
+const divSaludo = document.querySelector('#saludo')
 
-let proyecto = prompt ('ingrese el tipo de proyecto deseado para tu vivienda: proyectar o remodelar')
-const primerproyecto = 'remodelar'
-const segundoproyecto = 'proyectar'
+const divPresentacion = document.getElementById('presentacion')
 
-while ( (proyecto !== primerproyecto) && (proyecto !== segundoproyecto ) ){
+Swal.fire({
+    title: 'Bienvenido a ARKI!!',
+    text: 'Traenos tu idea, y dejá el resto en nuestras manos. Nosotros nos encargamos de todo: desde la planificación y diseño inicial, hasta la ejecución de obra y detalles finales. Todo lo que necesite tu proyecto!!',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+})
 
-    alert ('No contamos con este servicio.Vuelve a intentarlo!');
-    proyecto = prompt ('ingrese el tipo de proyecto deseado para tu vivienda: proyectar o remodelar');
 
-    arquitecto = prompt( 'Eliga uno de nuestros Arquitectos que ayuda en su proyecto: Agustin Lazaro - Lidia Gonzales - Alenjandro Diaz - Kiara Alfaro')
-    
-    }if (arquitecto === 'Agustin Lazaro' ){
 
-    alert ('Eligio al Arq. Agustin Lazaro. Se comunicaran proximamente')
 
-    } else if (arquitecto === 'Lidia Gonzales' ){
 
-        alert ('Eligio a la Arq. Lidia Gonzales. Se comunicaran proximamente')
-        
-    }else if (arquitecto === 'Alenjandro Diaz' ){
+botonIngresar.onclick = () => {
 
-        alert ('Eligio al Arq. Alenjandro Diaz. Se comunicaran proximamente')
-        
-    }else if (arquitecto === 'Kiara Alfaro' ){
+    const usuario = {
 
-        alert ('Eligio al Arq. Kiara Alfaro. Se comunicaran proximamente')
-        
-    }else if ((arquitecto !== 'Agustin Lazaro' ) && (arquitecto !== 'Lidia Gonzales' ) && (arquitecto !== 'Alenjandro Diaz' ) && (arquitecto !== 'Kiara Alfaro' ) ){
+        nombre: inputNombre.value,
+        apellido: inputApellido.value
 
-        alert ('El Arquitecto nombrado no es parte de nuestro equipo.')
-        
     }
 
+    localStorage.setItem('usuarioStorage',JSON.stringify(usuario))
+    divDatos.remove()
+    saludar(usuario)
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Ingreso correctamente'
+    })
+}
+
+function saludar(user){
+
+    const{nombre,apellido} = user
+
+    const saludarTitulo = document.createElement('h2')
+    saludarTitulo.innerText = `Bienvenido ${nombre} ${apellido},Ya puedes comenzar con tus compras!`
+    divSaludo.append(saludarTitulo)
+
+}
+
+///compras///
+
+
+let productosCocina = []
+let productosBano = []
+let carrito = []
+
+const seleccionCocina= document.getElementById('eleccionCocina')
+
+const seleccionBano = document.getElementById('eleccionBano')
+
+const terminarCompra = document.getElementById('terminarCompra')
+
+//--------- OBJETOS/SECTORES
+
+
+class sector {
+
+    constructor (m2, precio){
+
+        this.m2= m2
+        this.precio = precio
+    }
+}
+
+
+//cocina
 
 
 
+const cocina1 = new sector ('Entre 5m2 a 9m2', 8000);
+
+productosCocina.push(cocina1);
+
+const cocina2 = new sector ('Entre 9m2 a 15m2', 9000);
+
+productosCocina.push(cocina2);
+
+const cocina3 = new sector ('Mas de 15m2', 15000);
+
+productosCocina.push(cocina3);
 
 
+productosCocina.forEach(prodCocina=>{
 
-
-
-
-
-
-
-
-
-
-
-
-
+    const {m2, precio} = prodCocina
     
+    const option = document.createElement('option')
+    option.innerText = `${m2}: $${precio}`
+    seleccionCocina.append(option)
+})
+
+
+const agregarCocina= document.getElementById('agregarCocina')
+
+const botonAgregarCocina = document.createElement('button')
+botonAgregarCocina.innerText = 'Agregar al carrito'
+agregarCocina.append(botonAgregarCocina)
+
+agregarCocina.onclick = () => {
+
+    const sectoresSeleccionado = productosCocina[seleccionCocina.selectedIndex]
+    carrito.push(sectoresSeleccionado)
+
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Agregaste un producto al carrito',
+        showConfirmButton: false,
+        timer: 1500
+    })
+
+}
+
+
+//----------bano
+
+const bano1 = new sector ('Entre 5m2 a 9m2', 9000);
+
+productosBano.push(bano1);
+
+const bano2 = new sector ('Entre 9m2 a 15m2', 10000);
+
+productosBano.push(bano2);
+
+const bano3 = new sector ('Mas de 15m2', 20000);
+
+productosBano.push(bano3);
+
+
+
+productosBano.forEach(prodBano=>{
+
+    const {m2, precio} = prodBano
+
+    const option = document.createElement('option')
+    option.innerText = `${m2}: $${precio}`
+    seleccionBano.append(option)
+})
+
+const agregarBano = document.getElementById('agregarBano')
+
+const botonAgregarBano = document.createElement('button')
+botonAgregarBano.innerText = 'Agregar al carrito'
+agregarBano.append(botonAgregarBano)
+
+agregarBano.onclick = () => {
+
+    const sectoresSeleccionado = productosBano[seleccionBano.selectedIndex]
+    carrito.push(sectoresSeleccionado)
+
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Agregaste un producto al carrito',
+        showConfirmButton: false,
+        timer: 1500
+    })
+
+}
+
+
+
+
+//finalizar compra
+
+
+const botonFinalizar = document.getElementById('terminarCompra')
+const tituloMontoFinal = document.getElementById('tituloMontoFinal')
+
+botonFinalizar.onclick = () => {
+
+
+
+    let totalcompra = 0 
+
+    carrito.forEach((prod) => {
+
+        totalcompra = totalcompra + prod.precio
+    })
+
+    const tituloMonto = document.createElement('h2')
+    tituloMonto.innerText = `El precio final a pagar es de $ ${totalcompra},Gracias por su compra!`
+    tituloMontoFinal.append(tituloMonto)
+
+    divPresentacion.remove()
+    divSaludo.remove()
+
+}
+
+// insertar luego eleccion de arquitectos//
+
+
+
+
